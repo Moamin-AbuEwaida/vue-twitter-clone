fcabac91742bd38748a207271330b73d3de2d18e
Moamin <moamin.salamah@gmail.com>
2022-09-06 10:48:35 +0200

    1st commit, setting up the project with coding the leftside bar

---
+++ src/App.vue
@@ -0,0 +1,160 @@
+<script>
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      tabs: [
+        { icon: "fas fa-home", title: "Home", id: "home" },
+        { icon: "fas fa-hashtag", title: "Explore", id: "explore" },
+        { icon: "far fa-bell", title: "Notifications", id: "notifications" },
+        { icon: "far fa-envelope", title: "Messages", id: "messages" },
+        { icon: "far fa-bookmark", title: "Bookmark", id: "bookmark" },
+        { icon: "fas fa-clipboard-list", title: "Lists", id: "lists" },
+        { icon: "far fa-user", title: "Profile", id: "profile" },
+        { icon: "fas fa-ellipsis-h", title: "More", id: "more" },
+      ],
+      id: "home",
+      dropdown: false,
+      trending: [
+        {
+          top: "Trending in TX",
+          title: "Gigi",
+          bottom: "Trending with: Rip Gigi",
+        },
+        { top: "Music", title: "We Won", bottom: "135K Tweets" },
+        { top: "Pop", title: "Blue Ivy", bottom: "49k Tweets" },
+        { top: "Trending in US", title: "Denim Day", bottom: "35k Tweets" },
+        { top: "Trending", title: "When Beyonce", bottom: "20.5k Tweets" },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="app" class="flex container h-screen w-full">
+    <!-- side nav -->
+    <div
+      class="lg:w-1/5 border-r border-lighter lg:px-6 px-2 py-2 flex flex-col justify-between"
+    >
+      <div class="">
+        <button
+          class="h-12 w-12 hover:bg-lightblue text-3xl text-blue rounded-full"
+        >
+          <i class="fab fa-twitter"></i>
+        </button>
+        <div>
+          <button
+            v-for="(tab, i) in tabs"
+            :key="i"
+            @click="id = tab.id"
+            :class="`flex items-center py-2 px-4 hover:bg-lightblue hover:text-blue rounded-full mr-auto mb-3 focus:outline-none ${
+              id === tab.id ? 'text-blue' : ''
+            }`"
+          >
+            <i :class="`${tab.icon} text-2xl mr-4 text-left`"></i>
+            <p class="text-lg font-semibold text-left hidden lg:block">
+              {{ tab.title }}
+            </p>
+          </button>
+        </div>
+        <button
+          class="text-white bg-blue rounded-full font-semibold w-12 h-12 lg:w-full lg:h-auto p-3 focus:outline-none hover:bg-darkblue"
+        >
+          <p class="hidden lg:block">Tweet</p>
+          <i class="fas fa-plus lg:hidden"></i>
+        </button>
+      </div>
+      <div class="lg:w-full relative">
+        <button
+          @click="dropdown = true"
+          class="flex items-center w-full hover:bg-lightblue rounded-full focus:outline-none"
+        >
+          <img
+            src="./assets/images/profile1.png"
+            class="w-10 h-10 rounded-full border-lighter"
+          />
+          <div class="hidden lg:block ml-4">
+            <p class="text-sm font-bold leading-tight">xXx sniper</p>
+            <p class="text-sm leading-tight">@sniper</p>
+          </div>
+          <i class="fas fa-angle-down ml-auto text-lg"></i>
+        </button>
+        <div
+          v-if="dropdown === true"
+          class="hidden lg:block absolute bottom-0 left-0 mb-16 w-64 rounded-lg shadow-md border-lightest bg-white"
+        >
+          <button
+            @click="dropdown = false"
+            class="flex items-center w-full hover:bg-lightest p-3 focus:outline-none"
+          >
+            <img
+              src="./assets/images/profile1.png"
+              class="w-10 h-10 rounded-full border-lighter"
+            />
+            <div class="ml-4">
+              <p class="text-sm font-bold leading-tight">xXx sniper</p>
+              <p class="text-sm leading-tight">@sniper</p>
+            </div>
+            <i class="fas fa-check ml-auto text-blue"></i>
+          </button>
+          <button
+            @click="dropdown = false"
+            class="w-full text-left hover:bg-lightest border-t border-lighter p-3 text-sm focus:outline-none"
+          >
+            Add an existing account
+          </button>
+          <button
+            @click="dropdown = false"
+            class="w-full text-left hover:bg-lightest border-t border-lighter p-3 text-sm focus:outline-none"
+          >
+            Log out @sniper
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- tweets  -->
+    <div class="w-1/2 h-full"></div>
+    <!-- trending -->
+    <div
+      class="w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative"
+    >
+      <input
+        type="text"
+        class="pl-12 rounded-full w-full p-2 bg-lighter text-sm"
+        placeholder="Search Twitter"
+      />
+      <i
+        class="fas fa-search absolute left-0 top-0 mt-4 ml-12 text-sm text-light"
+      ></i>
+      <div class="w-full rounded-lg bg-lightest">
+        <div class="flex items-center justify-between p-3">
+          <p class="text-lg font-bold">Trends for You</p>
+          <i class="fas fa-cog text-lg text-blue"></i>
+        </div>
+        <button
+          v-for="(trend, i) in trending"
+          :key="i"
+          class="w-full flex justify-between hover:bg-lighter p-3 border-t border-lighter"
+        >
+          <div>
+            <p class="text-sm text-left leading-tight text-dark">
+              {{ trend.top }}
+            </p>
+            <p class="font-bold text-left leading-tight">{{ trend.title }}</p>
+            <p class="text-left leading-tight text-dark">{{ trend.bottom }}</p>
+          </div>
+          <i class="fas fa-angle-down text-lg text-dark"></i>
+        </button>
+        <button
+          class="p-3 w-full hover:bg-lighter text-left border-t border-lighter text-blue"
+        >
+          Show More
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  mode: "jit",
+  darkMode: false,
+  theme: {
+     container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var'],
+      },
+      colors: {
+        'blue': '#1DA1F2',
+        'darkblue': '#2795D9',
+        'lightblue': '#EFF9FF',
+        'dark': '#657786',
+        'light': '#AAB8C2',
+        'lighter': '#E1E8ED',
+        'lightest': '#F5F8FA',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
